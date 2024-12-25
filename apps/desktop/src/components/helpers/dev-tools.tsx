979468c380f8e91d1e86b7d14f49d94cdcda2d2a
Anas Mazouni <madadj4@gmail.com>
2024-12-25 06:04:30 +0100

    chore: dev tools should only show in dev mode

--- apps/desktop/src/components/helpers/dev-tools.tsx
+++ apps/desktop/src/components/helpers/dev-tools.tsx
@@ -1,13 +1,16 @@
 import { usePersistedStore } from '@/lib/store';
 import { invoke } from '@tauri-apps/api/core';
 import { Button } from '../ui/button';
+import TailwindIndicator from './tailwind-indicator';
 
 const DevTools = () => {
   const { clearMods } = usePersistedStore();
+  if (!import.meta.env.DEV) return null;
   return (
     <div className="flex gap-2">
       <Button onClick={clearMods}>Clear Mods</Button>
       <Button onClick={() => invoke('stop_game')}>Stop Game</Button>
+      <TailwindIndicator />
     </div>
   );
 };
