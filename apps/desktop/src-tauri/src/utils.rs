2f3179b3d0abe7ba9630bcc48435859c611d3a87
ArjixWasTaken <me@arjix.dev>
2025-01-24 21:43:14 +0200

    init support for linux

--- apps/desktop/src-tauri/src/utils.rs
+++ apps/desktop/src-tauri/src/utils.rs
@@ -1,10 +1,17 @@
 use crate::errors::Error;
 use std::process::Command;
 
-pub fn show_in_folder_windows(path: &str) -> Result<(), Error> {
+pub fn show_in_folder(path: &str) -> Result<(), Error> {
     #[cfg(target_os = "windows")]
     {
         match Command::new("explorer").arg(path).spawn() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(Error::FailedToOpenFolder(e.to_string())),
+        }
+    }
+    #[cfg(target_os = "linux")]
+    {
+        match Command::new("xdg-open").arg(path).spawn() {
             Ok(_) => Ok(()),
             Err(e) => Err(Error::FailedToOpenFolder(e.to_string())),
         }
