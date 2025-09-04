4e5202f1eca8b073719e4f83ef0df167a1427bf6
Anas Mazouni <madadj4@gmail.com>
2025-09-04 23:23:30 +0200

    fix(desktop): enable sorting in search results for improved relevance
    Resolves #41

--- apps/desktop/src/hooks/use-search.ts
+++ apps/desktop/src/hooks/use-search.ts
@@ -25,7 +25,7 @@ export const useSearch = <T = LocalMod>({ data, keys }: UseSearchProps<T>) => {
     () =>
       new Fuse(data, {
         keys: keys as FuseOptionKey<T>[],
-        shouldSort: false,
+        shouldSort: true,
         useExtendedSearch: true,
       }),
     [data, keys]
@@ -37,10 +37,7 @@ export const useSearch = <T = LocalMod>({ data, keys }: UseSearchProps<T>) => {
         return sortMods(data as LocalMod[], sortType);
       }
       const results = fuse.search(q);
-      return sortMods(
-        results.map((result) => result.item) as LocalMod[],
-        sortType
-      );
+      return results.map((result) => result.item) as LocalMod[];
     },
     [fuse, data, sortType]
   );
